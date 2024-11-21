65bf9d0e9e2efccd7e189bfeccaf442259c71e7d
Seesbastian <sebastianschwoeppe180@gmail.com>
2024-11-21 09:35:40 +0100

    translate explanation and generalize subdomain

--- sync-pub-ip-2-hetznerdns.py
+++ sync-pub-ip-2-hetznerdns.py
@@ -16,14 +16,14 @@ def update_dns_record(record_id, api_token, new_ip, zone_id):
     url = f"https://dns.hetzner.com/api/v1/records/{record_id}"
     headers = {
         "Content-Type": "application/json",
-        "Auth-API-Token": api_token  # API-Token in den Header aufnehmen
+        "Auth-API-Token": api_token  
     }
     data = {
-        "value": new_ip,  # Die neue IP-Adresse, die gesetzt werden soll
-        "ttl": 600,       # Time-to-Live für den DNS-Record
-        "type": "A",      # Typ des DNS-Records (A für IPv4)
-        "name": "nc",  # Name des DNS-Records (z.B. Subdomain)
-        "zone_id": zone_id  # Zone-ID, die die Domain angibt
+        "value": new_ip,  
+        "ttl": 600,       
+        "type": "A",      
+        "name": sub_domain,
+        "zone_id": zone_id 
     }
     try:
         response = requests.put(url, headers=headers, data=json.dumps(data))
@@ -37,9 +37,10 @@ def update_dns_record(record_id, api_token, new_ip, zone_id):
         print(f"HTTP Request failed: {e}")
 
 # Example
-record_id = "<Record-ID>"  # Hier die tatsächliche Record ID einfügen
-api_token = "<Hetzner-API-Token>"  # Dein API-Token
-new_ip = get_public_ip()  # Neue IP-Adresse, die du setzen willst
-zone_id = "<DNS-Zone>"  # Die Zone ID deiner Domain
+record_id = "<Record-ID>" # string of letters and numbers
+api_token = "<Hetzner-API-Token>"  # can be found in your hetzner account
+new_ip = get_public_ip()  # Your Public IP
+zone_id = "<DNS-Zone>"  # Zone of your domain
+sub_domain = "<your-subdomain>"
 
 update_dns_record(record_id, api_token, new_ip, zone_id)
